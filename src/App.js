baa80097135e6ca38c61053f323fc4f858049735
user-1289 <armaanzeyad@gmail.com>
2023-06-20 18:38:25 +0530

    adding signup feature to main todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,160 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import Login from './Login';
+import firebase from 'firebase/app';
+
+import { initializeApp} from "firebase/app";
+import { getFirestore, doc, setDoc, addDoc,collection, arrayUnion,where,getDocs, query, updateDoc,arrayRemove } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+
 
 // ...
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyACZG1v02NJ4upuFTCNk0u21OU9V4A8EJk",
+    authDomain: "todo-13753.firebaseapp.com",
+    projectId: "todo-13753",
+    storageBucket: "todo-13753.appspot.com",
+    messagingSenderId: "353536102896",
+    appId: "1:353536102896:web:e93212889223cf5c790d35"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
   const valRef = useRef([]);
   const indexRef = useRef(null);
   const [inputVal, setVal] = useState('');
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [user,setUser] = useState('')
+  const [todoArr,setTodoArr] = useState([])
+  //const [todoArr, setTodoArr] = useState(() => {
+  //  const savedTodos = localStorage.getItem('todos');
+  //  return savedTodos ? JSON.parse(savedTodos) : [];
+  //});
+ // useEffect(() =>
+ // {
+ //   if(!localStorage.getItem("key"))
+ //   {
+ //     alert('Sign in to use')
+ //     return
+ //   }
+ // }, [])
+let docRef = {};
+let key = '';
 
-  const [todoArr, setTodoArr] = useState(() => {
-    const savedTodos = localStorage.getItem('todos');
-    return savedTodos ? JSON.parse(savedTodos) : [];
-  });
+ useEffect(()=>
+ {
+  if(localStorage.getItem("UniqueKey"))
+  {
+    //console.log('damn it')
+    key = localStorage.getItem("UniqueKey")
+  docRef = doc(db, "todos", localStorage.getItem("UniqueKey"));
+  }
 
+ }, [])
+
+  async function getData()
+  {
+    let data;
+    const q = query(collection(db, 'todos'), where('UniqueId', '==', key));
+    const querySnapshot = await getDocs(q);
+    for (const doc of querySnapshot.docs) {
+       data = doc.data().TodoArr;
+    }
+    setTodoArr(data)
+  }
+  useEffect(()=>
+  {
+    if(localStorage.getItem("UniqueKey")!=null)
+    {
+      getData()
+    }
+  }, [])
+  //useEffect(()=>
+  //{
+  //  let userData = JSON.parse(localStorage.getItem("authState"))
+  //  console.log(userData.email)
+  //}, [])
+  //useEffect(() =>
+  //{
+  //  async function addTodos()
+  //  {
+  //    for(let i = 0; i < todoArr.length; i++)
+  //    {
+  //      await updateDoc(docRef,{
+  //        TodoArr:arrayUnion(todoArr[i])
+  //      })
+  //    }
+  //  }
+  // // addTodos()
+  //}, [])
   function updateVal(e) {
     let currentVal = e.target.value;
     setVal(currentVal);
   }
+  const auth = getAuth();
 
-  const saveText = () => {
+  useEffect(()=>
+  {
+    async function checkUser()
+    {
+     // let key = localStorage.getItem("UniqueKey")
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in
+          console.log('User is signed in:', user);
+          // Perform actions for signed-in user
+          // ...
+        } else {
+          // User is signed out
+          console.log('User is signed out');
+          // Perform actions for signed-out user
+          // ...
+        }
+      });
+
+    /*if(localStorage.getItem("UniqueKey")!=null)
+    {
+      console.log('it sthere')
+      const q = query(collection(db, 'todos'), where('UniqueId', '==', key));
+      const querySnapshot = await getDocs(q);
+      for (const doc of querySnapshot.docs) {
+        const data = doc.data().TodoArr;
+        console.log(data)
+      }
+
+    }*/
+    }
+    //checkUser()
+  }, [])
+ const saveText = async() => 
+  {
+    if(localStorage.getItem("UniqueKey")==null)
+    {
+      alert("You need to sign in to use this website")
+      setVal('')
+      return
+    }
+    //console.log(user)
+    if(localStorage.getItem("UniqueKey")!=null)
+    {
+      let nowDoc =  doc(db, "todos", localStorage.getItem("UniqueKey"));
+      //console.log(docRef)
+    await updateDoc(nowDoc,{
+      TodoArr:arrayUnion(inputVal)
+    })
+  }
+  //  let arrToSet = [...todoArr]
+  //  arrToSet.push(inputVal)
+   // await addDoc(collection(db, "todos"), {
+   // TodoArr: ["make chai boi", 'go code', 'eat', 'sleep']
+   // });
     setTodoArr([...todoArr, inputVal]);
     setVal('');
   };
@@ -47,10 +181,16 @@ export default function App() {
     localStorage.setItem('todos', JSON.stringify(newTodoArr));
   }
 
-  function deleteTodo(index) {
+ async function deleteTodo(index,valToDel) 
+  {
     const newTodoArr = [...todoArr];
     newTodoArr.splice(index, 1);
     setTodoArr(newTodoArr);
+    let nowDoc =  doc(db, "todos", localStorage.getItem("UniqueKey"));
+    //console.log(typeof nowDoc)
+    await updateDoc(nowDoc,{
+      TodoArr:arrayRemove(valToDel)
+    })
     localStorage.setItem('todos', JSON.stringify(newTodoArr));
   }
 
@@ -68,8 +208,10 @@ export default function App() {
   }
 
   return (
+    <>
+    <Login sendArr={obj => setTodoArr(obj)} userId={id=> setUser(id)}/>
     <center>
-      <div id="container">
+    <div id="container" >
         <h2>Enter a To Do</h2>
         <input
           onKeyDown={(e) => {
@@ -83,6 +225,7 @@ export default function App() {
           onChange={updateVal}
         /><br /><br />
         <button id="save-btn" onClick={saveText}>Save</button>
+        <hr />
         <DragDropContext onDragEnd={handleDragEnd}>
           <Droppable droppableId="todoList">
             {(provided) => (
@@ -103,7 +246,7 @@ export default function App() {
                           unique-id={index}
                           onChange={handleSpanClick}
                         />
-                        <span className="material-symbols-outlined" onClick={() => deleteTodo(index)}>delete</span>
+                        <span className="material-symbols-outlined" onClick={() => deleteTodo(index,todo)}>delete</span>
                         <span onClick={() => editTxt(index)} className="material-symbols-outlined">edit</span>
                         <hr className='hr-cl' />
                       </div>
@@ -117,6 +260,6 @@ export default function App() {
         </DragDropContext>
       </div>
     </center>
+    </>
   );
 }
-
---
+++ src/Login.js
@@ -0,0 +1,218 @@
+import React, { useEffect, useState } from 'react'
+import './login.css'
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider,setPersistence,browserLocalPersistence, signInWithRedirect, getRedirectResult,deleteUser } from "firebase/auth";
+import { getFirestore, doc, setDoc, addDoc,collection, arrayUnion, where,getDocs, query, deleteDoc} from "firebase/firestore";
+
+export default function Login(props) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyACZG1v02NJ4upuFTCNk0u21OU9V4A8EJk",
+        authDomain: "todo-13753.firebaseapp.com",
+        projectId: "todo-13753",
+        storageBucket: "todo-13753.appspot.com",
+        messagingSenderId: "353536102896",
+        appId: "1:353536102896:web:e93212889223cf5c790d35"
+      };
+      
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+
+      let userData;
+      const [userEmail,setUserEmail] = useState('')
+      const [userPhoto,setUserPhoto] = useState('')
+      const [showUser,setShowUser] = useState(false)
+      const [user,setUser] = useState(true)
+      const [userDetails,setUserDetails] = useState(false)
+      useEffect(()=>
+      {
+        if(localStorage.getItem("authState")!=null)
+        {
+          setShowUser(true)
+          setUser(false)
+        }
+            else if(localStorage.getItem("authState")==null)
+            {
+            setShowUser(false)
+            setUser(true)
+            }
+            
+      }, [])
+      const auth = getAuth();
+      let delUser = auth.currentUser
+      useEffect(()=>
+      {
+        if(localStorage.getItem("authState")!=null)
+        {
+          userData = JSON.parse(localStorage.getItem("authState"))
+          setUserEmail(userData.email)
+          setUserPhoto(userData.photoURL)
+        // console.log(userData.email)
+        }
+      }, [localStorage.getItem("authState")])
+
+      async function signToGoogle() {
+
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider(); // Create an instance of GoogleAuthProvider
+        
+        // Check if the user's authentication state is stored in persistent storage
+        const storedAuthState = localStorage.getItem('authState'); // You can use localStorage instead if desired
+      
+        if (storedAuthState) {
+          const user = JSON.parse(storedAuthState);
+         // console.log('User is already signed in:', user);
+          makeArr(user.uid);
+          return; // Exit the function since the user is already signed in
+        }
+        
+        await setPersistence(auth, browserLocalPersistence); // Set the persistence to local
+        
+        try {
+        //  signInWithRedirect(auth, provider);
+          const result = await signInWithPopup(auth, provider);
+        
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          
+          // The signed-in user info.
+          const user = result.user;
+         // console.log(user);
+          localStorage.setItem("UniqueKey",user.uid)
+          makeArr(user.uid);
+          setUser(false)
+          setShowUser(true)
+          // Store the user's authentication state in persistent storage
+          localStorage.setItem('authState', JSON.stringify(user)); // You can use localStorage instead if desired
+        
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        } catch (error) {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        }
+      }
+      
+      
+    
+      async function makeArr(unique) 
+      {
+        let arr = [];
+        const q = query(collection(db, 'todos'), where('UniqueId', '==', unique));
+        const querySnapshot = await getDocs(q);
+        for (const doc of querySnapshot.docs)
+        {
+          const data = doc.data();
+          arr.push(data);
+          if (arr.length > 0) 
+          {
+            if(localStorage.getItem("UniqueKey")!=null)
+            {
+             // console.log(localStorage.getItem("UniqueKey"))
+              getData()
+            }
+            //console.log('I am triggered');
+            return; // Exit the makeArr function if condition is met
+          }
+        }
+      
+       // const docRef = await addDoc(collection(db, 'todos'), 
+       // {
+       //   TodoArr: [],
+       //   UniqueId: unique
+       // });
+        const docRef = await setDoc(doc(db, "todos", unique), {
+          UniqueId:unique,
+          TodoArr:[]
+        })
+        //await setDoc(docRef, {
+        //  TodoArr: [],
+        //  UniqueId: unique
+        //});
+        
+       // const docId = docRef.id;
+      //localStorage.setItem("UniqueKey",unique)
+        props.userId(unique);
+      }
+      
+      function showDetails()
+      {
+        if(userDetails===false)
+        {
+          setUserDetails(true)
+        }
+          else if(userDetails===true)
+          {
+            setUserDetails(false)
+          }
+      }
+
+      function logUserOut()
+      {
+        setUserDetails(false)
+        setShowUser(false)
+        setUser(true)
+        localStorage.removeItem("authState")
+        localStorage.removeItem("UniqueKey")
+        localStorage.removeItem("todos")
+        props.sendArr([])
+      }
+
+      async function getData()
+      {
+        let data;
+        const q = query(collection(db, 'todos'), where('UniqueId', '==', localStorage.getItem("UniqueKey")));
+        const querySnapshot = await getDocs(q);
+        for (const doc of querySnapshot.docs) {
+           data = doc.data().TodoArr;
+        }
+        props.sendArr(data)
+        //localStorage.setItem("todos", JSON.stringify(data))
+      }
+
+      async function delUserOut()
+      {
+       let docRef = doc(db, "todos", localStorage.getItem("UniqueKey"));
+        await deleteDoc(docRef)
+        deleteUser(delUser).then(() => {
+          // User deleted.
+        }).catch((error) => {
+          // An error ocurred
+          // ...
+        });
+        setUserDetails(false)
+        setShowUser(false)
+        setUser(true)
+        localStorage.removeItem("authState")
+        localStorage.removeItem("UniqueKey")
+        localStorage.removeItem("todos")
+        props.sendArr([])
+      }
+      return (
+        <div className='login-container'>
+          {showUser && 
+          <div className='user-container'>
+            <span onClick={showDetails} style={{float:'right'}} class="material-symbols-outlined">expand_more</span>
+            <span className='user-email'>{userEmail}</span>
+          </div>}<br/>
+          {user && <div>
+          <span onClick={() => {signToGoogle()}} className='sign-txt'>Sign up</span>
+          <span onClick={() => {signToGoogle()}} className='log-txt'>Login</span>
+          </div>
+          }
+                      {userDetails && 
+            <div className='options-container'><br/>
+              <div onClick={logUserOut} className='text-user'>Log out</div><br/>
+              <div onClick={delUserOut} className='text-user'>Delete account</div>
+            </div>
+            }
+        </div>
+      );
+    }
